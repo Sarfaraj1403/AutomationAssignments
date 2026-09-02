1f0926fbd22b3fa1a996c9e2eda836a1f2168639
200~Sarfaraj1403 <sarfarajkool202@gmail.com>
2026-09-02 19:07:21 +0530

    inheritance and object

---
+++ src/com/creatio/crm/language/basics/Class1.java
@@ -0,0 +1,13 @@
+package com.creatio.crm.language.basics;
+
+public class Class1 {
+
+	String name="sarfaraj";
+	int roll = 45;
+	String nameg="iqra";
+	int rollg = 4;
+	
+	String[] student ={"ram" ,"sam","wok"};
+	int[] room = {1,2,3};
+
+}
---
+++ src/com/creatio/crm/language/basics/Class3.java
@@ -0,0 +1,15 @@
+package com.creatio.crm.language.basics;
+
+public class Class3 extends Class1 {
+
+	String company = "Accentor";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   
+           Class3 obj = new Class3();
+           System.out.println(obj.nameg);
+           System.out.println(obj.rollg);
+	}
+
+}
